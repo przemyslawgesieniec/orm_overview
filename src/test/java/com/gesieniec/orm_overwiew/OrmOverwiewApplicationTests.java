d60a12b580464279f4e386fe667261548ea64b18
Przemyslaw Gesieniec <Przemyslaw.Gesieniec@ttpsc.pl>
2019-05-25 20:48:33 +0200

    init

---
+++ src/test/java/com/gesieniec/orm_overwiew/OrmOverwiewApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gesieniec.orm_overwiew;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrmOverwiewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
